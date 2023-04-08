34849960de854ea4c7f89da9000e5a329026f401
WinikeCuamba <winikecuamba.github@gmail.com>
2023-04-08 14:49:38 +0000

    projecto comecado

---
+++ bot/client.mjs
@@ -0,0 +1,8 @@
+export class Client {
+    constructor(nome, email, token, state) {
+        this.nome = nome
+        this.email = email
+        this.token = token
+        this.state = state
+    }
+}
